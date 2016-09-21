fca803cb5137961f2aedb015ff35bc2d20a52c72
VVhuang <loveuu715@163.com>
2016-09-21 19:49:42 +0800

    initialize

--- app/src/main/java/com/loveuu/vv/SplashActivity.java
+++ app/src/main/java/com/loveuu/vv/SplashActivity.java
@@ -1,8 +1,41 @@
 package com.loveuu.vv;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.view.WindowManager;
+
 /**
  * Created by VV on 2016/9/21.
  */
 
-public class SplashActivity {
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        setFullScreen();
+        super.onCreate(savedInstanceState);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        }, 3000);
+    }
+
+    /**
+     * 设置全屏
+     */
+    public void setFullScreen() {
+        WindowManager.LayoutParams params = getWindow().getAttributes();
+        params.flags |= WindowManager.LayoutParams.FLAG_FULLSCREEN;
+        getWindow().setAttributes(params);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    }
+
 }
